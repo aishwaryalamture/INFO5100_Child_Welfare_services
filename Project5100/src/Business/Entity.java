0c87aa5da302d99cadef854f7ec3d8639d4ff141
aishwaryalamture <lamture.a@northeastern.edu>
2021-12-10 20:32:13 -0500

    removed validator from entity

--- Project5100/src/Business/Entity.java
+++ Project5100/src/Business/Entity.java
@@ -7,7 +7,6 @@ package Business;
 
 import Business.Enterprises.EnterpriseDirectory;
 import Business.Users.UserDirectory;
-import Business.Validator.Validator;
 import Business.WorkQueue.WorkQueue;
 import lombok.Getter;
 import lombok.Setter;
@@ -19,31 +18,28 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Entity {
-   
+
     private static Entity entity;
     private EnterpriseDirectory enterpriseDirectory;
     private UserDirectory userDirectory;
-    private Validator validator;
     private WorkQueue workQueue;
 
-    public Entity(EnterpriseDirectory enterpriseDirectory, UserDirectory userDirectory, Validator validator, WorkQueue workQueue) {
+    public Entity(EnterpriseDirectory enterpriseDirectory, UserDirectory userDirectory, WorkQueue workQueue) {
 
         this.enterpriseDirectory = enterpriseDirectory;
         this.userDirectory = userDirectory;
-        this.validator = validator;
-        this.workQueue=workQueue;
+        this.workQueue = workQueue;
     }
-    
-    private Entity(){
+
+    private Entity() {
         enterpriseDirectory = new EnterpriseDirectory();
         userDirectory = new UserDirectory();
-        validator = new Validator();
         workQueue = new WorkQueue();
     }
-    
-    public static Entity getInstance(){
-        if(entity==null){
-            entity=new Entity();
+
+    public static Entity getInstance() {
+        if (entity == null) {
+            entity = new Entity();
         }
         return entity;
     }
